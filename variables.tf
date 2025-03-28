f12463429de2efe196325c6e51eb23baf566abe9
priyulp <93876688+priyulp@users.noreply.github.com>
2025-03-28 11:05:42 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "project_id" {}
+variable "region" { default = "us-central1" }
+variable "vpc_name" { default = "my-vpc" }
+variable "subnet_name" { default = "my-subnet" }
+variable "vm_name" { default = "my-vm" }
+variable "bucket_name" { default = "my-storage-bucket" }
