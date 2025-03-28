0e5bd8a2dda73a1ac244b1f1673c3edfa54ea47d
priyulp <93876688+priyulp@users.noreply.github.com>
2025-03-28 16:50:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "vpc" {
-  source      = "./module/vpc"
+  source      = "./modules/vpc"
   vpc_name    = var.vpc_name
   subnet_name = var.subnet_name
 }
 
 module "vm" {
-  source   = "./module/vm"
+  source   = "./modules/vm"
   vm_name  = var.vm_name
   subnet   = module.vpc.subnet_self_link
 }
 
 module "storage" {
-  source      = "./module/storage"
+  source      = "./modules/storage"
   bucket_name = var.bucket_name
 }
