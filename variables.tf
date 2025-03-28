0a262241d7c417d4ae010f23060e5547bc10bb49
priyulp <93876688+priyulp@users.noreply.github.com>
2025-03-28 15:54:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "project_id" {}
+variable "project_id" {copper-cider-453013-b7}
 variable "region" { default = "us-central1" }
 variable "vpc_name" { default = "my-vpc" }
 variable "subnet_name" { default = "my-subnet" }
