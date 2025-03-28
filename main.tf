e82ed407ec2159240fac6e0cad5b4a4ddc55187e
priyulp <93876688+priyulp@users.noreply.github.com>
2025-03-28 11:13:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source      = "./modules/vpc"
+  vpc_name    = var.vpc_name
+  subnet_name = var.subnet_name
+}
+
+module "vm" {
+  source   = "./modules/vm"
+  vm_name  = var.vm_name
+  subnet   = module.vpc.subnet_self_link
+}
+
+module "storage" {
+  source      = "./modules/storage"
+  bucket_name = var.bucket_name
+}
