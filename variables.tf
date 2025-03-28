6a8027e90d5e6a4ee910dc07ebcf17823be5f6f3
priyulp <93876688+priyulp@users.noreply.github.com>
2025-03-28 17:34:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,35 @@
-variable "project_id" = "copper-cider-453013-b7" 
-variable "region" { default = "us-central1" }
-variable "vpc_name" { default = "my-vpc" }
-variable "subnet_name" { default = "my-subnet" }
-variable "vm_name" { default = "my-vm" }
-variable "bucket_name" { default = "my-storage-bucket" }
+variable "project_id" {
+  default     = "copper-cider-453013-b7"
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  default     = "us-central1"
+  description = "Region for GCP resources"
+  type        = string
+}
+
+variable "vpc_name" {
+  default     = "my-vpc"
+  description = "VPC name"
+  type        = string
+}
+
+variable "subnet_name" {
+  default     = "my-subnet"
+  description = "Subnet name"
+  type        = string
+}
+
+variable "vm_name" {
+  default     = "my-vm"
+  description = "Virtual Machine name"
+  type        = string
+}
+
+variable "bucket_name" {
+  default     = "my-storage-bucket"
+  description = "GCS bucket name"
+  type        = string
+}
